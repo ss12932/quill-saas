226b7adc5b2ec56b3348d66a32d0d59ecb292fb3
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-02 22:05:27 +0100

    animation effect for onboarding steps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,10 +85,49 @@ export default function Home() {
             </p>
           </div>
         </div>
-        {/* steps */}
+        {/* Steps */}
         <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1 ">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:-top-0.5 after:left-0 after:bg-blue-600 after:origin-bottom-left after:transition-transform after:hover:scale-x-100 after:rounded-sm after:duration-300">
+              <span className="text-sm font-medium text-blue-600">Step 1</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 relative text-zinc-700">
+                Either starting out with a free plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
           <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300"></div>
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:-top-0.5 after:left-0 after:bg-blue-600 after:origin-bottom-left after:transition-transform after:hover:scale-x-100 after:rounded-sm after:duration-300">
+              <span className="text-sm font-medium text-blue-600">Step 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:-top-0.5 after:left-0 after:bg-blue-600 after:origin-bottom-left after:transition-transform after:hover:scale-x-100 after:rounded-sm after:duration-300">
+              <span className="text-sm font-medium text-blue-600">Step 3</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple. Try out Quill today - it really takes
+                less than a minute.
+              </span>
+            </div>
           </li>
         </ol>
       </div>
