cd7da2be8c5799b1275ca2e6f05f3e140aeb0276
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-02 14:27:40 +0100

    mw wrapper + clsx/tw util

---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,15 @@
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
