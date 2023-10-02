d0b867f123642c52fb8c541259de147ab4743b3c
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-02 19:00:58 +0100

    shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -9,6 +11,17 @@ export default function Home() {
           Quill is now public!
         </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-blue-600">documents</span> in
+        seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        Quill allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away!
+      </p>
+      <Link href="/dashboard" target="_blank">
+        Get started <ArrowRight className="ml-2 h-5 w-5" />
+      </Link>
     </MaxWidthWrapper>
   );
 }
