f3d32f4321fe6c39f1554a1eadd4f579fb759990
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-03 19:30:00 +0100

    trpc setup check

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,19 @@
-import { router } from "./trpc";
+import { NextResponse } from "next/server";
+import { publicProcedure, router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
 
-const appRouter = router({});
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    return { success: true };
+  }),
+});
 
 export type AppRouter = typeof appRouter;
