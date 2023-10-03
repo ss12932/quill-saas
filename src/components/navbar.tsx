78dc7a33d9b51446b3ee4d418474f70c1ba4c015
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-03 19:01:52 +0100

    trpc setup + kinde auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./max-width-wrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -10,6 +13,24 @@ const Navbar = () => {
             <span>quill.</span>
           </Link>
           {/* TODO: add mobile navbar */}
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/"
+                className={buttonVariants({ variant: "ghost", size: "sm" })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({ variant: "ghost", size: "sm" })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink className={buttonVariants({ size: "sm" })}>
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
+            </>
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
