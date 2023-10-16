540981cab66dcea867ee9982cfb6ad775e38b301
ss12932 <ss12932@my.bristol.ac.uk>
2023-10-16 15:03:29 +0100

    uploadthing setup

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -6,14 +6,43 @@ import { Button } from "@/components/ui/button";
 
 import Dropzone from "react-dropzone";
 import { File, UploadCloud } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
 
 const UploadDropzone = () => {
   const [isUploading, setIsUploading] = useState<boolean>(true);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        return prevProgress + 5;
+      });
+    }, 500);
+
+    return interval;
+  };
+
   return (
     <Dropzone
       multiple={false}
-      onDrop={(acceptedFile) => {
-        console.log(acceptedFile);
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+
+        const progressInterval = startSimulatedProgress();
+
+        // handle file uploading
+
+        // mock upload time
+        // await new Promise((resolve) => setTimeout(resolve, 15000));
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -45,7 +74,12 @@ const UploadDropzone = () => {
                 </div>
               ) : null}
               {isUploading ? (
-                <div className="w-full mt-4 max-w-xs mx-auto"></div>
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress
+                    value={uploadProgress}
+                    className="h-1 w-full bg-zinc-200"
+                  />
+                </div>
               ) : null}
             </label>
           </div>
